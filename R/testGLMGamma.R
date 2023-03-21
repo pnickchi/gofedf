ba8702033d6b7c0ab3b5c052acc680fa120f90a3
pnickchi <payman.nickchi@gmail.com>
2023-03-21 16:35:51 -0700

    minor update

--- R/testGLMGamma.R
+++ R/testGLMGamma.R
@@ -84,7 +84,7 @@ testGLMGamma = function(x, y, l, fit = NULL, start.value = NULL, control = NULL,
       stop('The family in fit must be Gamma.')
     }
 
-    if( !inherits(fit,'x') ){
+    if( !is.matrix(fit$x) ){
       stop('fit object must have the design matrix corresponding to the model. Consider setting x = TRUE in glm function to return x matrix.')
     }
 
